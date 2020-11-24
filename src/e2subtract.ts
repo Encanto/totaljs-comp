a427177e9ed0d533aec152df0a3178cf83631f0b
WilliamRClark <cwilliamwork@yahoo.com>
2020-11-24 09:18:55 -0500

    Add subtract component.

---
+++ src/e2subtract.ts
@@ -0,0 +1,48 @@
+import math = require('mathjs');
+
+const ID = 'e2subtract';
+
+exports.id = 'e2subtract';
+exports.title = 'Subtract';
+exports.group = 'Math';
+exports.color = '#08a351';
+exports.version = '1.0.0';
+exports.author = 'William Clark';
+exports.icon = 'minus-circle';  // Icon reference from https://fontawesome.com/v4.7.0/icons/
+exports.input = true;
+exports.output = 1;
+exports.options = { enabled: true };
+exports.readme = `# Subtract Numbers
+
+Subtract numeric values recieved from each other, and outputs total.`;
+
+exports.html = `<div class="padding">
+	<div data-jc="textbox" data-jc-path="initialvalue" data-jc-config="placeholder:0;increment:true;type:number;align:center">@(Initial Value)</div>
+</div>`.format(ID);
+
+exports.install = function(instance: any) {
+
+	var count: math.MathType = 0.0;
+
+	instance.on('data', function(data: any) {
+        console.log('data recieved:' + JSON.stringify(data));
+        count = math.subtract(count, data.data);
+		instance.custom.status();
+	});
+
+	instance.custom.stats = function(callback: ICallback) {
+	};
+
+
+	instance.custom.status = function() {
+		setTimeout2(instance.id, function() {
+			instance.status('total: ' + count.toString());
+			instance.send2(count);
+		}, 100);
+	};
+
+};
+
+exports.uninstall = function() {
+	FLOW.trigger(ID, null);
+};
